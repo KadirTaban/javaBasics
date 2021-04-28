aaefd3eba0c5c6d27a3b3af302b6aaff9890dd04
Kadir Taban <32621058+KadirTaban@users.noreply.github.com>
2021-04-29 00:53:56 +0300

    OOP

--- src/oopIntro/oopIntro.java
+++ src/oopIntro/oopIntro.java
@@ -10,7 +10,21 @@ public class oopIntro {
         product1.unitPrice= 15000;
         product1.detail = "16 GB RAM";
 
-        Product[] products={product1};
+
+        Product product2= new Product();//ornek olusturma,referans olusturma,instance olusturma
+        product1.id =2 ;
+        product1.name="Lenovo V15";
+        product1.unitPrice= 16000;
+        product1.detail = "32 GB RAM";
+
+        Product product3= new Product();//ornek olusturma,referans olusturma,instance olusturma
+        product1.id =3 ;
+        product1.name=" Hp ";
+        product1.unitPrice= 8000;
+        product1.detail = "8 GB RAM";
+
+        Product[] products={product1,product2,product3};
+
 
         System.out.println(products.length);
 
