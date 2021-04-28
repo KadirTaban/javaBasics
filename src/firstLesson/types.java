1d9f1896f580a3c3e17d856ee4c7bc8386847e96
Kadir Taban <32621058+KadirTaban@users.noreply.github.com>
2021-04-28 23:30:01 +0300

    FirstClass

--- src/firstLesson/types.java
+++ src/firstLesson/types.java
@@ -2,7 +2,67 @@ package firstLesson;
 
 public class types {
     public static void main(String[] args){
-        System.out.println("Hello Java !");
+
+        String internetSubeButonu = "İnternet Şubesi";
+
+        //System.out.println(internetSubeButonu);
+        //System.out.println(internetSubeButonu);
+        //System.out.println(internetSubeButonu);
+        //System.out.println(internetSubeButonu);
+        double dolarDun = 8.15;
+        double dolarBugun = 8.10 ;
+        int vade = 36 ;
+        boolean dustuMu = false;
+        System.out.println(internetSubeButonu);
+        if(dolarBugun<dolarDun){
+            System.out.println("*Dolar düştü resmi*");
+
+        }else if(dolarBugun>dolarDun){
+            System.out.println("*Dolar arttı resmi*");
+
+        }else{
+            System.out.println("Dolar eşittir resmi");
+        }
+
+        String kredi1="Hızlı Kredi";
+        String kredi2="Mutlu Emekli Kredisi";
+        String kredi3 = "Konut Kredisi";
+        String kredi4= "Çiftçi Kredisi";
+        String kredi5="Taşıt Kredisi";
+
+        System.out.println(kredi1);
+        System.out.println(kredi2);
+        System.out.println(kredi3);
+        System.out.println(kredi4);
+        System.out.println(kredi5);
+
+        String[] krediler = {
+                    "Hızlı Kredi",
+                    "Mutlu Emekli Kredisi",
+                    "Konut Kredisi",
+                    "Çiftçi Kredisi",
+                    "Taşıt Kredisi"
+        };
+        //foreach
+        for(String kredi : krediler) {
+            System.out.println(kredi);
+        }
+        for(int i= 0 ; i<krediler.length;i++){
+            System.out.println(krediler[i]);
+        }
+        //System.out.println("Hello Java !");
+        int sayi1= 10 ;
+        int sayi2= 20 ;
+        sayi1 = sayi2;
+        sayi2 = 100;
+        System.out.println(sayi1);
+        //20 bilenlerdeniz.. Value Types !!
+        int[] sayilar1= {1,2,3,4,5};
+        int[] sayilar2= {10,20,30,40,50};
+        sayilar1= sayilar2;
+        sayilar2[0] = 100;
+        System.out.println(sayilar1[0]);
+        //100 , refree types
 
     }
 }
