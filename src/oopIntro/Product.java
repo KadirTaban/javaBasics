601f64a1ee3bffb93a88414821c0738fb168de8c
Kadir Taban <32621058+KadirTaban@users.noreply.github.com>
2021-04-29 01:28:31 +0300

    oop

--- src/oopIntro/Product.java
+++ src/oopIntro/Product.java
@@ -1,6 +1,14 @@
 package oopIntro;
 
 public class Product {
+    //Constructor
+    public Product() {
+        System.out.println("Ben çalıştım");
+    }
+    public Product(int id, String name,double unitPrice, String detail) {
+
+    }
+
     int id;
     String name;
     double unitPrice;
--- src/oopIntro/oopIntro.java
+++ src/oopIntro/oopIntro.java
@@ -11,22 +11,33 @@ public class oopIntro {
         product1.detail = "16 GB RAM";
 
 
-        Product product2= new Product();//ornek olusturma,referans olusturma,instance olusturma
-        product1.id =2 ;
-        product1.name="Lenovo V15";
-        product1.unitPrice= 16000;
-        product1.detail = "32 GB RAM";
+        Product product2= new Product();
+        product2.id =2 ;
+        product2.name="Lenovo V15";
+        product2.unitPrice= 16000;
+        product2.detail = "32 GB RAM";
 
-        Product product3= new Product();//ornek olusturma,referans olusturma,instance olusturma
-        product1.id =3 ;
-        product1.name=" Hp ";
-        product1.unitPrice= 8000;
-        product1.detail = "8 GB RAM";
+        Product product3= new Product();
+        product3.id =3 ;
+        product3.name=" Hp ";
+        product3.unitPrice= 8000;
+        product3.detail = "8 GB RAM";
 
         Product[] products={product1,product2,product3};
 
+        for (Product product: products) {
+            System.out.println(product.name);
+        }
 
         System.out.println(products.length);
+
+        Category category1 = new Category();
+        category1.id=1;
+        category1.name="Bilgisayar";
+
+        Category category2 = new Category();
+        category2.id=1;
+        category2.name="Ev/Bahçe";
 
     }
 }
