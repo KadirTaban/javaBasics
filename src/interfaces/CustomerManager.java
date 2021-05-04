4a6a5490de0105d306dba62a14cbaefd988bda32
Kadir Taban <32621058+KadirTaban@users.noreply.github.com>
2021-05-04 03:05:15 +0300

    interface

---
+++ src/interfaces/CustomerManager.java
@@ -0,0 +1,28 @@
+package interfaces;
+
+public class CustomerManager {
+
+    private Logger[] loggers;
+
+    public  CustomerManager(Logger[] loggers) {
+        this.loggers=loggers;
+    }
+    //loosly -tightly coupled
+    public void add(Customer customer) {
+        System.out.println("Müşteri eklendi" + customer.getFirstName());
+        utils.runLoggers(loggers, customer.getFirstName());
+
+
+        }
+
+
+
+
+    public void delete(Customer customer) {
+        System.out.println("Müşteri silindi"+customer.getFirstName());
+
+        utils.runLoggers(loggers, customer.getLastName());
+
+        }
+    }
+
